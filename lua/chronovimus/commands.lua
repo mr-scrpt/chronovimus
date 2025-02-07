1ea791413bb7fdd64125b3f821c612ff634d80d4
mr.scrpt <mr.scrpt@gmail.com>
2025-02-07 18:56:26 +0200

    feat: telescope add

--- lua/chronovimus/commands.lua
+++ lua/chronovimus/commands.lua
@@ -3,20 +3,21 @@ local history = require("chronovimus.history")
 local M = {}
 
 function M.setup()
-  vim.api.nvim_create_user_command("HistoryBack", function()
-    history.navigate_back()
-  end, {})
+	vim.api.nvim_create_user_command("HistoryBack", function()
+		history.navigate_back()
+	end, {})
 
-  vim.api.nvim_create_user_command("HistoryForward", function()
-    history.navigate_forward()
-  end, {})
+	vim.api.nvim_create_user_command("HistoryForward", function()
+		history.navigate_forward()
+	end, {})
 
-  vim.api.nvim_create_user_command("HistoryDebug", function()
-    history.debug_history()
-  end, {})
+	vim.api.nvim_create_user_command("HistoryDebug", function()
+		history.debug_history()
+	end, {})
 
-  vim.keymap.set("n", "<leader>bp", ":HistoryBack<CR>", { silent = true })
-  vim.keymap.set("n", "<leader>bn", ":HistoryForward<CR>", { silent = true })
+	vim.keymap.set("n", "<leader>bp", ":HistoryBack<CR>", { silent = true })
+	vim.keymap.set("n", "<leader>bn", ":HistoryForward<CR>", { silent = true })
+	vim.keymap.set("n", "<leader>bl", ":HistoryList<CR>", { silent = true })
 end
 
 return M
---
+++ lua/chronovimus/telescope.lua
@@ -0,0 +1,44 @@
+local M = {}
+
+function M.show_history_in_telescope()
+	local pickers = require("telescope.pickers")
+	local finders = require("telescope.finders")
+	local actions = require("telescope.actions")
+	local action_state = require("telescope.actions.state")
+	local conf = require("telescope.config").values
+
+	local history = require("history_nav.history").get_history()
+	if #history == 0 then
+		print("История пуста")
+		return
+	end
+
+	pickers
+		.new({}, {
+			prompt_title = "History",
+			finder = finders.new_table({
+				results = history,
+				entry_maker = function(entry)
+					return {
+						value = entry,
+						display = entry,
+						ordinal = entry,
+					}
+				end,
+			}),
+			sorter = conf.generic_sorter({}),
+			attach_mappings = function(prompt_bufnr, map)
+				local function open_selected()
+					local selection = action_state.get_selected_entry()
+					actions.close(prompt_bufnr)
+					vim.cmd("edit " .. vim.fn.fnameescape(selection.value))
+				end
+				map("i", "<CR>", open_selected)
+				map("n", "<CR>", open_selected)
+				return true
+			end,
+		})
+		:find()
+end
+
+return M
